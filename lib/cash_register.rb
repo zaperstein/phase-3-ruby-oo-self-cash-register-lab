e6a9bf2aa59e87120b6bb864748b95e8ac473dce
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-03 13:38:41 -0500

    commit tree f3bf232d4b1574ecc824da34c09d6aba006260e6

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  attr_accessor :total, :discount, :title, :price, :quantity
+  def initialize(discount=0)
+    @total=0
+    @discount=discount
+  end
 
+  def add_item(title, price, quantity=1)
+    self.total += quantity*price
+  end
+  
+  def apply_discount(discount)
+    # case discount
+    #turn discount number into percentage
+    percentage_discount = 1 - (discount/100).round
+    case percentage_discount
+    when percentage_discount != 0
+      self.total * percentage_discount
+    else
+      self.total
+    end
+  end
+end
